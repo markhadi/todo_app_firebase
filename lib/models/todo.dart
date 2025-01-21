83a45585dacf3c86342532226340f60a9f847f0b
Markhadi <markhadi@protonmail.com>
2025-01-21 15:26:35 +0800

    Creating a Todo model with the fromMap, toMap, and copyWith methods

---
+++ lib/models/todo.dart
@@ -0,0 +1,44 @@
+class Todo {
+  final String id;
+  final String title;
+  final String description;
+  final bool isCompleted;
+
+  Todo({
+    required this.id,
+    required this.title,
+    required this.description,
+    this.isCompleted = false,
+  });
+
+  factory Todo.fromMap(Map<String, dynamic> data, String documentId) {
+    return Todo(
+      id: documentId,
+      title: data['title'] ?? '',
+      description: data['description'] ?? '',
+      isCompleted: data['isCompleted'] ?? false,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'title': title,
+      'description': description,
+      'isCompleted': isCompleted,
+    };
+  }
+
+  Todo copyWith({
+    String? id,
+    String? title,
+    String? description,
+    bool? isCompleted,
+  }) {
+    return Todo(
+      id: id ?? this.id,
+      title: title ?? this.title,
+      description: description ?? this.description,
+      isCompleted: isCompleted ?? this.isCompleted,
+    );
+  }
+}
